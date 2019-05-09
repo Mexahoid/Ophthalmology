37814968a7a94420a667dc4ffb352a164ff0d393
Mexahoid <mexahoidbloodwing@Gmail.com>
2019-05-09 16:58:43 +0300

    Alpha 1.0 - добавлено сохранение картинок с параметрами

--- Ophthalmology/EyeLogics/EyeTextWindow.xaml.cs
+++ Ophthalmology/EyeLogics/EyeTextWindow.xaml.cs
@@ -7,10 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Cursors = System.Windows.Input.Cursors;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
 
 namespace Ophthalmology.EyeLogics
 {
@@ -23,7 +26,7 @@ namespace Ophthalmology.EyeLogics
         private BitmapImage _img;
         private Color _textColor = Colors.Black;
         private string _currentText;
-        private List<TextBlock> _textblocks;
+        private readonly List<TextBlock> _textblocks;
         private TextBlock _currentBlock;
         private bool _isDragging;
         private Point _lastPos;
@@ -117,9 +120,40 @@ namespace Ophthalmology.EyeLogics
 
         private void SaveClick(object sender, RoutedEventArgs e)
         {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "PNG картинки (*.png)|*.png|JPEG изображения (*.jpg)|*.jpg";
+                sfd.InitialDirectory = _img.UriSource.AbsolutePath;
+                if (sfd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                    return;
+                Rect bounds = VisualTreeHelper.GetDescendantBounds(MainArea);
+                double dpi = 96d;
 
-            DialogResult = true;
-            Close();
+                RenderTargetBitmap rtb = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, System.Windows.Media.PixelFormats.Default);
+
+                DrawingVisual dv = new DrawingVisual();
+                using (DrawingContext dc = dv.RenderOpen())
+                {
+                    VisualBrush vb = new VisualBrush(MainArea);
+                    dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+                }
+
+                rtb.Render(dv);
+                //endcode as PNG
+                //var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, (int)rect.Right, (int)rect.Bottom));
+                BitmapEncoder pngEncoder = new PngBitmapEncoder();
+                pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+                pngEncoder.Save(ms);
+                ms.Close();
+                System.IO.File.WriteAllBytes(sfd.FileName, ms.ToArray());
+                DialogResult = true;
+                Close();
+            }
+
+
+
         }
 
         private void CancelClick(object sender, RoutedEventArgs e)
@@ -161,19 +195,18 @@ namespace Ophthalmology.EyeLogics
 
         private void MoveTextBlock(object sender, MouseEventArgs e)
         {
-            if (_isDragging)
-            {
-                var pos = Mouse.GetPosition(MainArea);
-                double dx = pos.X - _lastPos.X;
-                double dy = pos.Y - _lastPos.Y;
+            if (!_isDragging)
+                return;
+            var pos = Mouse.GetPosition(MainArea);
+            double dx = pos.X - _lastPos.X;
+            double dy = pos.Y - _lastPos.Y;
 
-                Point relativePoint = _currentBlock.TransformToAncestor(MainArea)
-                    .Transform(new Point(0, 0));
+            Point relativePoint = _currentBlock.TransformToAncestor(MainArea)
+                .Transform(new Point(0, 0));
 
-                Canvas.SetLeft(_currentBlock, relativePoint.X + dx);
-                Canvas.SetTop(_currentBlock, relativePoint.Y + dy);
-                _lastPos = pos;
-            }
+            Canvas.SetLeft(_currentBlock, relativePoint.X + dx);
+            Canvas.SetTop(_currentBlock, relativePoint.Y + dy);
+            _lastPos = pos;
         }
     }
 }
